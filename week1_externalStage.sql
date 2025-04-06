fd03736118ec3ff788d9fd301757338e0fd90edc
MrRipository <wshdisjdu.5sos@gmail.com>
2025-04-06 10:40:04 +0900

    Update week1_externalStage.sql

--- week1_externalStage.sql
+++ week1_externalStage.sql
@@ -3,6 +3,7 @@ CREATE SCHEMA frozen_schema;
 
 USE DATABASE frozen_db;
 USE SCHEMA frozen_schema;
+USE warehouse compute_wh;
 
 CREATE STAGE aws_s3
 URL = 's3://frostyfridaychallenges/challenge_1/';
