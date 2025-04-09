ff6dfe733e4e802c70877754b02ea00d9a0e31ef
MrRipository <wshdisjdu.5sos@gmail.com>
2025-04-10 07:13:20 +0900

    Update week1_externalStage.sql

--- week1_externalStage.sql
+++ week1_externalStage.sql
@@ -1,9 +1,13 @@
-CREATE DATABASE frozen_db;
-CREATE SCHEMA frozen_schema;
+CREATE DATABASE ff_db;
+CREATE SCHEMA ff_schema;
 
-USE DATABASE frozen_db;
-USE SCHEMA frozen_schema;
+USE DATABASE ff_db;
+USE SCHEMA ff_schema;
 USE warehouse compute_wh;
 
-CREATE STAGE aws_s3
+CREATE STAGE ff_week1
 URL = 's3://frostyfridaychallenges/challenge_1/';
+
+CREATE OR REPLACE TABLE TBL (Col1 varchar);
+
+COPY INTO tbl from @ff_week1;
